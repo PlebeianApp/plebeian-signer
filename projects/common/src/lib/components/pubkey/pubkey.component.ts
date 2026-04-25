f63e300b469939c0eb95ea1f74a715ebed213579
hkarani <hezronkaranii@gmail.com>
2026-04-25 06:02:58 +0300

    fix: get nostr pubkey object

--- projects/common/src/lib/components/pubkey/pubkey.component.ts
+++ projects/common/src/lib/components/pubkey/pubkey.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { NostrHelper } from '@common';
 import { IconButtonComponent } from "../icon-button/icon-button.component";
 
@@ -9,7 +9,7 @@ import { IconButtonComponent } from "../icon-button/icon-button.component";
   templateUrl: './pubkey.component.html',
   styleUrl: './pubkey.component.scss',
 })
-export class PubkeyComponent implements OnInit {
+export class PubkeyComponent implements OnInit, OnChanges {
   @Input({ required: true }) value!: string;
   @Input() first = 9;
   @Input() last = 5;
@@ -19,13 +19,13 @@ export class PubkeyComponent implements OnInit {
   npubString: string | undefined;
 
   ngOnInit(): void {
-    const pubkeyObject = NostrHelper.getNostrPubkeyObject(this.value);
-    this.npub = pubkeyObject.npub;
-    this.npubString = NostrHelper.splitKey(
-      pubkeyObject.npub,
-      this.first,
-      this.last
-    );
+    this.resolvePubkey();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['value'] || changes['first'] || changes['last']) {
+      this.resolvePubkey();
+    }
   }
 
   copyToClipboard() {
@@ -34,5 +34,27 @@ export class PubkeyComponent implements OnInit {
     }
 
     navigator.clipboard.writeText(this.npub);
+  }
+
+  private resolvePubkey(): void {
+    const input = (this.value ?? '').trim();
+    if (!input) {
+      this.npub = undefined;
+      this.npubString = '';
+      return;
+    }
+
+    try {
+      const pubkeyObject = NostrHelper.getNostrPubkeyObject(input);
+      this.npub = pubkeyObject.npub;
+      this.npubString = NostrHelper.splitKey(
+        pubkeyObject.npub,
+        this.first,
+        this.last
+      );
+    } catch {
+      this.npub = undefined;
+      this.npubString = 'invalid pubkey';
+    }
   }
 }
